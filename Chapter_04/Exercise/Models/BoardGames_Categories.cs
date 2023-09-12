77d6337a34b64bf7ea6704cbad1caea5190bbc73
Muhammed ali Erdel <nagonad@gmail.com>
2023-09-12 19:49:53 +0200

    Chapter 4 exercises added

---
+++ Chapter_04/Exercise/Models/BoardGames_Categories.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyBGList.Models
+{
+    public class BoardGames_Categories
+    {
+        [Key]
+        [Required]
+        public int BoardGameId { get; set; }
+
+        [Key]
+        [Required]
+        public int CategoryId { get; set; }
+
+        [Required]
+        public DateTime CreatedDate { get; set; }
+
+
+        public BoardGame? BoardGame { get; set; }
+        public Category? Category { get; set; }
+    }
+}
---
+++ Chapter_04/Exercise/Models/Category.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyBGList.Models
+{
+    [Table("Categories")]
+    public class Category
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime CreatedDate { get; set; }
+
+        [Required]
+        public DateTime LastModifiedDate { get; set; }
+
+        public ICollection<BoardGames_Categories>? BoardGames_Categories { get; set; }
+    }
+}
